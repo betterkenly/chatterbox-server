a7bc510c1dc62166d025a798a0be7df83baa3c45
Kenly Hui <better_kenly@Kenlys-iMac.local>
2017-06-07 22:58:13 -0700

    edited

--- server/request-handler.js
+++ server/request-handler.js
@@ -14,79 +14,28 @@ this file and include it in basic-server.js so that it actually works.
 
 
 
-var url = require('url');
+  var url = require('url');
 
-var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  // headers['Content-Type'] = 'JSON';
-// console.log(request);
-  var results = [];
-  var url = '/classes/messages';
-  if (request.method === 'GET') {
-    
-    if (request.url !== url) {
-      response.writeHead(404, {'Content-Type': 'text/html'});
-      response.write('');
-      response.end();
+  var objectId = 2;
 
-    } else {
-      let dataObj = {
-        results: results
-      };
-      // console.log(request);
-      response.writeHead(200, {'Content-Type': 'application/json'});
-      response.end(JSON.stringify(dataObj));
+  var messages = [ 
+    {
+      text: 'first message',
+      userName: 'kenly',
+      objectId: 1
     }
-    
-  } else if (request.method === 'POST') {
-    if (request.url === url) {
-      var data = request._postData;
-      results.push(request._postData);
-      response.writeHead(201, {})
-  }
-    
-    
+  ];
 
+  var headers = defaultCorsHeaders;
+
+  var url = '/classes/messages';
   
-
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code. 
-  // url.resolve('http://127.0.0.1:3000', 'classes/messages');
-  
-
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
   
 
   // The outgoing status.
   var statusCode = 200;
 
-  // See the note below about CORS headers.
-  var headers = {
-    'access-control-allow-origin': '*',
-    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'access-control-allow-headers': 'content-type, accept',
-    'access-control-max-age': 10 // Seconds.
-  };
-
-  response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -95,9 +44,17 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('OK');
-};
 
+  var defaultCorsHeaders = {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10 // Seconds.
+  };
+
+var requestHandler = function(request, response) {
+  
+};
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
@@ -107,11 +64,6 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-// var defaultCorsHeaders = {
-//   'access-control-allow-origin': '*',
-//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-//   'access-control-allow-headers': 'content-type, accept',
-//   'access-control-max-age': 10 // Seconds.
-// };
+
 exports.requestHandler = requestHandler;
-// module.exports.defaultCorsHeaders = defaultCorsHeaders;
+
